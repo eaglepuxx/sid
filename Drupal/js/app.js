ab563b17abca86a4c9d1252b9b108a40db6e96d8
eaglepuxx <mikehansenmikehansen@jahns.dk>
2015-05-07 13:31:27 +0200

    sad

---
+++ Drupal/js/app.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+	jQuery(".region.region-page-top.well").append('<img src="http://www.mildred.co/issueassets/22/14RSowlhat.jpg" />');
+	jQuery(".region.region-page-top.well img").css({ "height": "60px", "width": "60px" });
+	jQuery("#block-search-form").appendTo('.region-page-top');
+	jQuery("#block-search-form").css({"float": "right", "width": "250px"});  
+});
